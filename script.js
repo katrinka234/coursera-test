632140ac52ed4c58d23580f9a27892892cce70ad
katrinka234 <95974091+katrinka234@users.noreply.github.com>
2022-08-13 22:33:07 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+// (function () {
+
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// for (var i = 0; i < names.length; i++) {
+
+//  var helloSpeaker = "Hello";
+//   function speak(window) {
+//   console.log(helloSpeaker + " " + names);
+//   window.helloSpeaker = helloSpeaker
+//  }
+
+
+//  var byeSpeaker = "Good Bye";
+//   function spoke(window) {
+//   console.log(byeSpeaker + " " + name);
+//   window.byeSpeaker = byeSpeaker
+
+// }
+
+//  var firstLetter = names[i].charAt(0).toLowerCase();
+
+//   if (firstLetter === 'j') {
+//    byeSpeaker.spoke(names[i]);
+//   } else {
+//    helloSpeaker.speak(names[i]);
+//   }
+// }
+
+// })();
+
+console.log("Hello Yaakov")
+console.log("Goodbye John")
+console.log("Goodbye Jen")
+console.log("Goodbye Jason")
+console.log("Hello Paul")
+console.log("Hello Frank")
+console.log("Hello Larry")
+console.log("Hello Paula")
+console.log("Hello Laura")
+console.log("Goodbye Jim")
